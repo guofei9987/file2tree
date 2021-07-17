71e0504e79172e85653992c1723447db3eff0fd8
guofei9987 <guofei9987@foxmail.com>
2021-07-18 00:37:24 +0800

    for macbook

--- mydir.py
+++ mydir.py
@@ -1,27 +1,14 @@
 import os
-def my_listdir(leval,path):
 
+
+def my_listdir(leval, path):
     for i in os.listdir(path):
-        print('|  '*(leval + 1) + i)
-        if os.path.isdir(path+i):
-
-            my_listdir(leval+1, path+i+"\\")
-
-
-path = 'D:\py'#+os.sep+'ant'
-print(path+os.sep)
-my_listdir(0, path+os.sep)
+        print('|  ' * (leval + 1) + i)
+        if os.path.isdir(path + i):
+            my_listdir(leval + 1, path + i + "\\")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+path = 'D:\py'  # +os.sep+'ant'
+print(path + os.sep)
+my_listdir(0, path + os.sep)
+import spacy
